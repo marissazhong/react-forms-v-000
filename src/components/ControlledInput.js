51f1f6f1fa6eeb339cb3520108922362e94c86f3
Marissa Zhong <mwz2101@columbia.edu>
2018-08-07 12:51:57 +0000

    automatically backed up by learn

--- src/components/ControlledInput.js
+++ src/components/ControlledInput.js
@@ -5,12 +5,12 @@ class Form extends Component {
     firstName: "John",
     lastName: "Henry"
   }
- 
+
   render() {
     return (
       <form>
-        <input type="text" name="firstName" value={this.state.firstName} />
-        <input type="text" name="lastName" value={this.state.lastName} />
+      <input type="text" id="firstName" onChange={event => this.handleFirstNameChange(event)} value={this.state.firstName} />
+      <input type="text" id="lastName" onChange={event => this.handleLastNameChange(event)} value={this.state.lastName} />
       </form>
     )
   }
